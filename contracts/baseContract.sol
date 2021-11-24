16c6f4a250f489da8575d6b6f9798b9e4919f3eb
ari4325 <ari4325@live.com>
2021-11-24 06:54:54 +0530

    Updated solidity files

--- contracts/baseContract.sol
+++ contracts/baseContract.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.8.9;
 contract baseContract {
-    struct userStruct{
+    struct User{
+        uint flag;
         string name;
         string mobile_no;
         address payable account;
@@ -8,12 +9,25 @@ contract baseContract {
         uint256 payment_amt;
     }
 
-    mapping (address => userStruct) users;
+    struct Update{
+        address _address;
+        uint256 timestamp;
+        string update;
+        User user;
+    }
+
+    event userAdded(address user_address, address sender);
+    event setUpdate(address sender);
+
+    mapping (address => User) users;
     address payable [] public addressUsers;
 
-    constructor(string name, string mobile_no, address payable _address) public {
-        userStruct storage user = users[_address];
+    Update [] updates;
 
+    constructor(string memory name, string memory mobile_no, address payable _address) {
+        User storage user = users[_address];
+
+        user.flag = 1;
         user.name = name;
         user.mobile_no = mobile_no;
         user.id = 1;
@@ -23,72 +37,46 @@ contract baseContract {
         addressUsers.push(_address);
     }
 
-  struct updateStruct{
-    address _address;
-    uint256 timestamp;
-    string update;
-  }
+    function userExists(address payable _address) public view returns (bool){
+        if(users[_address].flag == 0){
+            return false;
+        }
+        return true;
+    }
 
-  function addUser(address payable _address, string memory name, string memory mobile_no, uint256 )
+    function addUser(address payable _address, string memory name, string memory mobile_no, uint id, uint256 payment_amt) public returns (string memory){
+        if(userExists(_address)){
+            User storage user = users[_address];
 
-  mapping (address => logiStruct) logisticsProviders;
-  address payable [] public addressLogistics;
+            user.flag = 1;
+            user.name = name;
+            user.mobile_no = mobile_no;
+            user.id = id;
+            user.payment_amt = payment_amt;
+            user.account = _address;
+
+            addressUsers.push(_address);
+
+            emit userAdded(_address, msg.sender);
+            return "User added";
+        }
+        else{
+            return "User not found";
+        }
+    }
+
+    function setUpdates(string memory _update) public {
+        Update memory update;
+        update._address = msg.sender;
+        update.timestamp = block.timestamp;
+        update.update = _update;
+        update.user = users[msg.sender];
+
+        emit setUpdate(msg.sender);
+        updates.push(update);
+    }
   
-  //adding a logistics provider to the contract
-  function addLogisticsProvider(address payable _address, string memory name, string memory location, uint payment_amt) public {
-      logiStruct storage logi = logisticsProviders[_address];
-      
-      logi.name = name;
-      logi.location = location;
-      logi.payment_amt = payment_amt;
-      
-      addressLogistics.push(_address);
-  }
-  
-  //getting addresses of the logistics providers included in our contract.
-  function getLogisticsAddresses() external view returns(address payable [] memory) {
-      return addressLogistics;
-  }
-  
-  //fetching name and location of logistics Provider who were registered on the contract
-  function getDataLogistics(address _address) external view returns(string memory, string memory){
-      return (logisticsProviders[_address].name, logisticsProviders[_address].location);
-  }
-  
-  function addEther() external payable{}
-  
-  //checking value present on contract
-  function getBalanceOnContract() external view returns(uint){
-    return address(this).balance;
-  }
-  
-  function hasSufficientFunds() external view returns(bool){
-      uint amt_payable = 0;
-      for(uint i = 0; i<addressLogistics.length; i++){
-          address a = addressLogistics[i];
-          amt_payable =  amt_payable + logisticsProviders[a].payment_amt;
-      }
-      uint available_amt = address(this).balance;
-      return available_amt>=amt_payable;
-  }
-  
-  
-  //handling payments below
-  function payEther(address payable _address, uint amt) external{
-      _address.transfer(amt);
-  }
-  function releasePayment() external{
-      if(this.hasSufficientFunds()){
-          for(uint i = 0; i<addressLogistics.length; i++){
-              address payable a = addressLogistics[i];
-              a.transfer(logisticsProviders[a].payment_amt);
-          }
-      }
-  }
-  
-  updateStruct[] updates;
-  function setUpdate(address _address, string memory _update) external {
-      updateStruct memory update = updateStruct(_address, block.timestamp, _update);
-      updates.push(update);
-  }
+    function addEther() external payable{}
+
 }
+
